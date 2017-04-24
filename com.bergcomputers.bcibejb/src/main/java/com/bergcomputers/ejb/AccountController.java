1b07ce47ce137c9d9cad96576e7a82a02687d1f1
ancau2 <ancaungureanu01@gmail.com>
2017-04-24 19:54:07 +0300

    Working on Account Test

---
+++ com.bergcomputers.bcibejb/src/main/java/com/bergcomputers/ejb/AccountController.java
@@ -0,0 +1,70 @@
+package com.bergcomputers.ejb;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+
+import com.bergcomputers.domain.Account;
+
+@Stateless
+public class AccountController extends AbstractController<Account,Long> implements IAccountController{
+
+	EntityManager em;
+	
+	public AccountController(){
+		super(Account.class);
+	}
+	
+	@Override
+	protected EntityManager getEntityManager() {
+		return em;
+	}
+	
+	@Override
+	public Account find(long id){
+		return getEntityManager().find(Account.class, id);
+	}
+	
+	@Override
+	public List<Account> findAll() {
+		return getEntityManager().createQuery("select c from Account c").getResultList();
+	}
+	
+	@Override
+	public List<Account> findRange(int startPosition, int size) {
+		CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+		cq.select(cq.from(entityClass));
+		Query q = getEntityManager().createQuery(cq);
+		q.setMaxResults(size);
+		q.setFirstResult(startPosition);
+		return q.getResultList();
+	}
+	
+	@Override
+	public void delete(long accountid) {
+
+		Account item = find(accountid);
+		if (item != null) {
+			getEntityManager().remove(item);
+		}
+	}
+	
+	@Override
+	public Account create(Account account) {
+		if (null != account && null == account.getCreationDate()) {
+			account.setCreationDate(new Date());
+		}
+		getEntityManager().persist(account);
+		getEntityManager().flush();
+		return account;
+	}
+
+	@Override
+	public Account update(Account account) {
+		return getEntityManager().merge(account);
+	}
+}
