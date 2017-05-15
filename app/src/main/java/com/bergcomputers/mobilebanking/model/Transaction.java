681ff3ba4ccdf91eee42e629b28922eb7820fef3
saddas <sadsadsa>
2017-05-15 20:02:19 +0300

    TransactionActivity+ layout

---
+++ app/src/main/java/com/bergcomputers/mobilebanking/model/Transaction.java
@@ -0,0 +1,200 @@
+package com.bergcomputers.mobilebanking.model;
+
+/**
+ * Created by berg on 5/15/2017.
+ */
+import android.accounts.Account;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+/**
+ * Entity implementation class for Entity: Transaction
+ *
+ */
+
+
+public class Transaction extends BaseEntity implements Serializable, ITransaction  {
+
+    public static final String FIELD_DATE="date";
+    public static final String FIELD_TRANSACTION_TYPE="type";
+    public static final String FIELD_AMOUNT="amount";
+    public static final String FIELD_SENDER="sender";
+    public static final String FIELD_DETAILS="details";
+    public static final String FIELD_STATUS="status";
+
+    private static final long serialVersionUID = -7944505705705785135L;
+    public final static String findAll = "com.bergcomputers.domain.transaction.findAll";
+
+
+
+    private Date date;
+
+
+    private Account account;
+
+    private String transactionType;
+    private Double amount;
+    private String sender;
+    private String details;
+    private String status;
+
+    public Transaction(){
+        super();
+    }
+
+
+
+
+
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public Date getTransactionDate() {
+
+        return this.date;
+    }
+    @Override
+    public void setTransactionDate(Date Date) {
+
+        this.date=Date;
+    }
+    @Override
+    public String getTransactionType() {
+
+        return this.transactionType;
+    }
+    @Override
+    public void setTransactionType(String type) {
+
+        this.transactionType=type;
+    }
+    @Override
+    public Double getAmount() {
+
+        return this.amount;
+    }
+    @Override
+    public void setAmount(Double amount) {
+
+        this.amount=amount;
+    }
+    @Override
+    public String getSender() {
+
+        return this.sender;
+    }
+    @Override
+    public void setSender(String sender) {
+
+        this.sender=sender;
+    }
+    @Override
+    public String getDetails() {
+        // TODO Auto-generated method stub
+        return this.details;
+    }
+    @Override
+    public void setDetails(String details) {
+        // TODO Auto-generated method stub
+        this.details=details;
+    }
+    @Override
+    public String getStatus() {
+        // TODO Auto-generated method stub
+        return this.status;
+    }
+    @Override
+    public void setStatus(String status) {
+        // TODO Auto-generated method stub
+        this.status=status;
+    }
+
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((account == null) ? 0 : account.hashCode());
+        result = prime * result + ((amount == null) ? 0 : amount.hashCode());
+        result = prime * result + ((date == null) ? 0 : date.hashCode());
+        result = prime * result + ((details == null) ? 0 : details.hashCode());
+        result = prime * result + ((sender == null) ? 0 : sender.hashCode());
+        result = prime * result + ((status == null) ? 0 : status.hashCode());
+        result = prime * result + ((transactionType == null) ? 0 : transactionType.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Transaction other = (Transaction) obj;
+        if (account == null) {
+            if (other.account != null)
+                return false;
+        } else if (!account.equals(other.account))
+            return false;
+        if (amount == null) {
+            if (other.amount != null)
+                return false;
+        } else if (!amount.equals(other.amount))
+            return false;
+        if (date == null) {
+            if (other.date != null)
+                return false;
+        } else if (!date.equals(other.date))
+            return false;
+        if (details == null) {
+            if (other.details != null)
+                return false;
+        } else if (!details.equals(other.details))
+            return false;
+        if (sender == null) {
+            if (other.sender != null)
+                return false;
+        } else if (!sender.equals(other.sender))
+            return false;
+        if (status == null) {
+            if (other.status != null)
+                return false;
+        } else if (!status.equals(other.status))
+            return false;
+        if (transactionType == null) {
+            if (other.transactionType != null)
+                return false;
+        } else if (!transactionType.equals(other.transactionType))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction [date=" + date + ", account=" + account + ", type="
+                + transactionType + ", amount=" + amount + ", sender=" + sender
+                + ", details=" + details + ", status=" + status + "]";
+    }
+
+
+}
---
+++ app/src/main/java/com/bergcomputers/mobilebanking/transaction/TransactionListActivity.java
@@ -0,0 +1,213 @@
+package com.bergcomputers.mobilebanking.transaction;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bergcomputers.mobilebanking.R;
+import com.bergcomputers.mobilebanking.common.Util;
+import com.bergcomputers.mobilebanking.common.activity.BaseActivity;
+import com.bergcomputers.mobilebanking.common.net.IJSONNetworkActivity;
+import com.bergcomputers.mobilebanking.common.net.JSONAsyncTask;
+import com.bergcomputers.mobilebanking.currency.CurrencyDetailFragment;
+import com.bergcomputers.mobilebanking.model.Currency;
+import com.bergcomputers.mobilebanking.model.Transaction;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * An activity representing a list of Currencies. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link CurrencyDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class TransactionListActivity extends BaseActivity implements IJSONNetworkActivity{
+
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        // Show the Up button in the action bar.
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        //View recyclerView = findViewById(R.id.currency_list);
+        //assert recyclerView != null;
+
+        //load the data from the backend
+        new JSONAsyncTask(Util.BASE_URL+ Util.URL_GET_CURRENCIES, this, 0).execute();
+
+        //setupRecyclerView((RecyclerView) recyclerView);
+
+        if (findViewById(R.id.transaction_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. Use NavUtils to allow users
+            // to navigate up one level in the application structure. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void handleResult(String pJSONString, int currentAction) {
+        if (null != pJSONString){
+            List<Transaction> currencies = new ArrayList<>();
+            try {
+                JSONArray jsonArray = new JSONArray(pJSONString);
+                for(int i=0; i< jsonArray.length();i++){
+                    JSONObject jsonObj = (JSONObject)jsonArray.get(i);
+                    Transaction transaction = new Transaction();
+                    transaction.setTransactionType(jsonObj.getLong(Transaction.FIELD_TRANSACTION_TYPE));
+                    transaction.setDate(jsonObj.getString(Transaction.FIELD_DATE));
+                    transaction.setAmount(jsonObj.getDouble(Transaction.FIELD_AMOUNT));
+                    transaction.setSender(jsonObj.getDouble(Transaction.FIELD_SENDER));
+                    transaction.setAmount(jsonObj.getDouble(Transaction.FIELD_AMOUNT));
+                    transaction.setDetails(jsonObj.getDouble(Transaction.FIELD_DETAILS));
+                    transaction.setStatus(jsonObj.getDouble(Transaction.FIELD_STATUS));
+
+                    currencies.add(transaction);
+
+                }
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            View recyclerView = findViewById(R.id.transaction_list);
+            assert recyclerView != null;
+
+            ((RecyclerView)recyclerView).setAdapter(new SimpleItemRecyclerViewAdapter(currencies));
+        }
+    }
+
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final List<Transaction> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<Transaction> items) {
+            mValues = items;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.transaction_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.mIdView.setText(mValues.get(position).getDate());
+            holder.mContentView.setText(mValues.get(position).getExchangerate().toString());
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mTwoPane) {
+                        Bundle arguments = new Bundle();
+                        arguments.putString(TransactionDetailFragment.ARG_ITEM_ID, holder.mItem.getId().toString());
+                        TransactionDetailFragment fragment = new TransactionDetailFragment();
+                        fragment.setArguments(arguments);
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.transaction_detail_container, fragment)
+                                .commit();
+                    } else {
+                        Context context = v.getContext();
+                        Intent intent = new Intent(context, TransactionDetailActivity.class);
+                        intent.putExtra(TransactionDetailFragment.ARG_ITEM_ID, holder.mItem.getId());
+
+                        context.startActivity(intent);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mIdView;
+            public final TextView mContentView;
+            public Transaction mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mIdView = (TextView) view.findViewById(R.id.id);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mContentView.getText() + "'";
+            }
+        }
+    }
+}
