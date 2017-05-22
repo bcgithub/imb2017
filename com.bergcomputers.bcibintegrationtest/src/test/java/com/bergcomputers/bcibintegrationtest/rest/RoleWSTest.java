e7d2c03cff63156ff6ba80f61c693c99be434e47
mirau13 <miraursu13@gmail.com>
2017-05-22 19:52:29 +0300

    Role Tests Complete

--- com.bergcomputers.bcibintegrationtest/src/test/java/com/bergcomputers/bcibintegrationtest/rest/RoleWSTest.java
+++ com.bergcomputers.bcibintegrationtest/src/test/java/com/bergcomputers/bcibintegrationtest/rest/RoleWSTest.java
@@ -6,6 +6,7 @@ package com.bergcomputers.bcibintegrationtest.rest;
 
 import static javax.ws.rs.client.Entity.json;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 
@@ -80,16 +82,19 @@ public class RoleWSTest extends AbstractTest {
         params.put("size", 1);
         
 		List<Role> roles = target(serviceRelativePath, params).accept(jsonFormat).get(genericListType);
-		assertEquals(1, roles.size());
-		assertEquals(created1.getId(), roles.get(0).getId());
-        params.put("page", 2);
-        roles = target(serviceRelativePath, params).accept(jsonFormat).get(genericListType);
-		assertEquals(1, roles.size());
-		assertEquals(created2.getId(), roles.get(0).getId());
-		
-		//Deleting test currency
-		deleteRole(created1.getId());
-		deleteRole(created2.getId());
+		try{
+			assertEquals(1, roles.size());
+			assertEquals(created1.getId(), roles.get(0).getId());
+	        params.put("page", 2);
+	        roles = target(serviceRelativePath, params).accept(jsonFormat).get(genericListType);
+			assertEquals(1, roles.size());
+			assertEquals(created2.getId(), roles.get(0).getId());
+		}finally{
+			
+			//Deleting test currency
+			deleteRole(created1.getId());
+			deleteRole(created2.getId());
+		}
 
 	}
 
@@ -241,4 +246,145 @@ public class RoleWSTest extends AbstractTest {
 	private void deleteRole(Long id){
 		target(serviceRelativePath + id).delete();
 	}
+	
+	@Test
+	@RunAsClient
+	public void deleteRoleTestNoId() {
+		// Creating test currency
+	//	createRole();
+		
+		// existing currencies
+		List<Role> roles = getRoles();
+		
+		
+		//delete test currency
+		target(serviceRelativePath + "-1").delete();
+		
+		// new currencies list
+		List<Role> rolesNewList = getRoles();
+		
+		//check the list size to be decrease by one
+		assertEquals(roles.size(), rolesNewList.size() );
+	}
+	
+	@Test
+	@RunAsClient
+	public void createRoleTestNoCreationDate() {
+				
+		//existing currencies
+		List<Role> roles = getRoles();
+
+		//Creating test currency
+		Role role = createRoleEntity();
+		role.setCreationDate(null);
+		Response resp = target(serviceRelativePath).post(json(role));
+		Role created = resp.readEntity(Role.class);
+
+		//Getting list of currencies
+		List<Role> rolesNewList = getRoles();
+		
+		//check the list size to be increased by one
+		assertEquals(roles.size() +1, rolesNewList.size() );
+		
+		assertEquals(role.getName(), created.getName());
+
+		assertNotNull(created.getCreationDate());
+		
+		//Deleting test currency
+		deleteRole(rolesNewList.get(0).getId());
+
+	}
+	@Test
+	public void createRoleNullTest() {
+		Role role = null;
+		
+		try{
+			roleContoller.create(role);
+			fail("If ex not thrown should fail");
+		}catch(Exception e){
+			
+		}
+	}
+	
+	@Test
+//	@RunAsClient
+	public void getRolesPaginationTestPageMin() {
+		Role created1 = createRole();
+		Role created2 = createRole();
+		Map<String, Object> params = new HashMap<>();
+        params.put("page", -3);
+        params.put("size", 1);
+        
+		List<Role> roles = target(serviceRelativePath, params).accept(jsonFormat).get(genericListType);
+		try{
+			assertEquals(2, roles.size());
+			assertEquals(created1.getId(), roles.get(0).getId());
+		}finally{
+			//Deleting test currency
+			deleteRole(created1.getId());
+			deleteRole(created2.getId());
+		}
+	}
+	
+	@Test
+//	@RunAsClient
+	public void getRolesPaginationTestPageNull() {
+		Role created1 = createRole();
+		Role created2 = createRole();
+		Map<String, Object> params = new HashMap<>();
+        params.put("page", null);
+        params.put("size", 1);
+        
+		List<Role> roles = target(serviceRelativePath, params).accept(jsonFormat).get(genericListType);
+		try{
+			assertEquals(2, roles.size());
+			assertEquals(created1.getId(), roles.get(0).getId());
+		}finally{
+			//Deleting test currency
+			deleteRole(created1.getId());
+			deleteRole(created2.getId());
+		}
+	}
+	
+	@Test
+
+	public void getRolesPaginationTestSizeNull() {
+		Role created1 = createRole();
+		Role created2 = createRole();
+		Map<String, Object> params = new HashMap<>();
+        params.put("page", 1);
+        
+		List<Role> roles = target(serviceRelativePath, params).accept(jsonFormat).get(genericListType);
+		try{
+			assertEquals(2, roles.size());
+			assertEquals(created1.getId(), roles.get(0).getId());
+		}finally{
+			//Deleting test currency
+			deleteRole(created1.getId());
+			deleteRole(created2.getId());
+		}
+
+
+	}
+	
+	@Test
+//	@RunAsClient
+	public void getRolesPaginationTestSizeMin() {
+		Role created1 = createRole();
+		Role created2 = createRole();
+		Map<String, Object> params = new HashMap<>();
+        params.put("page", 1);
+        params.put("size", -3);
+        
+		List<Role> roles = target(serviceRelativePath, params).accept(jsonFormat).get(genericListType);
+		try{
+			assertEquals(2, roles.size());
+			assertEquals(created1.getId(), roles.get(0).getId());
+		}finally{
+			//Deleting test currency
+			deleteRole(created1.getId());
+			deleteRole(created2.getId());
+		}
+	}
+	
 }
